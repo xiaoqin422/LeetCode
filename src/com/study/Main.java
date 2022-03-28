a41ca6d856513d126ed3d6e26427efdf3ef33917
Dove <2578908933@qq.com>
2022-03-28 15:46:52 +0800

    初始提交

---
+++ src/com/study/Main.java
@@ -0,0 +1,16 @@
+package com.study;
+
+/**
+ * 包名: com.study
+ * 类名: Main
+ * 创建用户: 25789
+ * 创建日期: 2022年03月28日 15:39
+ * 项目名: LeetCode
+ *
+ * @author: 秦笑笑
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
